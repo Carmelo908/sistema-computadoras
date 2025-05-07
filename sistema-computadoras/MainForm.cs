6e518d1976cdb2785e72959153fb7a3a1db87770
Carmelo908 <carmelopepigauna@gmail.com>
2025-05-07 09:44:07 -0300

    created project

---
+++ sistema-computadoras/MainForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+	
+namespace Trimestre
+{
+  public static class GUIFactory
+  {
+    
+    public static Button ButtonFactory(string text)
+    {
+      Button button = new Button();
+      button.Text = text;
+      return button;
+    }
+
+  }
+	
+  public sealed partial class MainForm : Form
+  {
+    public MainForm()
+    {
+      Initialize();
+      Button button = GUIFactory.ButtonFactory("ALGOOOO");
+      button.Location = new Point(10, 10);
+      this.Controls.Add(button);
+    }
+		
+    private void Initialize()
+    {
+      this.Text = "Formulario"; 
+      this.Size = new Size(600, 400); 
+      this.StartPosition = FormStartPosition.CenterScreen; 
+      this.BackColor = Color.White;
+    }
+  }
+}
---
+++ sistema-computadoras/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: User
+ * Date: 28/4/2025
+ * Time: 15:45
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Windows.Forms;
+
+namespace Trimestre
+{
+	internal sealed class Program
+	{
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	}
+}
